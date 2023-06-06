8d2e19fac5021c5c543efc6cd136a3d0be6b27b9
Taerande <revive0405@gmail.com>
2023-06-06 17:41:32 +0900

    single_thread

---
+++ single_thread.py
@@ -0,0 +1,32 @@
+import os
+import time
+import threading
+
+
+count = 0  # 전역 변수 초기화
+
+print('------Single Thread-----')
+
+def increment():
+    global count
+    while count < 3000000:
+        count += 1
+    print(f"Thread ID: {threading.get_ident()}, Process ID: {os.getpid()}")
+
+# 총 소요 시간 측정을 위한 시작 시간 기록
+start_time = time.time()
+
+# 5개의 스레드 생성 및 실행
+threads = []
+for _ in range(1):
+    thread = threading.Thread(target=increment)
+    thread.start()
+    threads.append(thread)
+
+# 모든 스레드의 실행 종료를 대기
+for thread in threads:
+    thread.join()
+
+# 결과 출력
+print("Count:", count)
+print("Total Elapsed Time:", time.time() - start_time)
