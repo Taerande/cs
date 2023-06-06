966d5512441b1fee4d0ca3fcfe16945ae5d62973
Taerande <revive0405@gmail.com>
2023-06-07 08:39:36 +0900

    count value 오류 수정

--- multi_process.py
+++ multi_process.py
@@ -7,12 +7,13 @@ count = Value('i', 0)  # 공유 변수 초기화
 print('-------Multi Process------')
 
 def increment(lock):
-    lock.acquire()
     global count
-    while count < 3000000:
+    while count.value < 3000000:
+        lock.acquire()
         count.value += 1
-    lock.release()
+        lock.release()
     print(f"Process ID: {os.getpid()}")
+
 
 # 총 소요 시간 측정을 위한 시작 시간 기록
 start_time = time.time()
