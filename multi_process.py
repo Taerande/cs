c94da6837232a2fea0123d694cf02e98b75192f0
Taerande <revive0405@gmail.com>
2023-06-06 20:35:41 +0900

    newly updated

--- multi_process.py
+++ multi_process.py
@@ -3,15 +3,15 @@ import time
 from multiprocessing import Process, Value, Lock
 
 count = Value('i', 0)  # 공유 변수 초기화
-lock = Lock()  # 프로세스 동기화를 위한 Lock 객체
 
 print('-------Multi Process------')
 
-def increment():
+def increment(lock):
+    lock.acquire()
     global count
-    while count.value < 3000000:
-        with lock:
-            count.value += 1
+    while count < 3000000:
+        count.value += 1
+    lock.release()
     print(f"Process ID: {os.getpid()}")
 
 # 총 소요 시간 측정을 위한 시작 시간 기록
@@ -20,7 +20,8 @@ start_time = time.time()
 # 3개의 프로세스 생성 및 실행
 processes = []
 for _ in range(3):
-    process = Process(target=increment)
+    lock = Lock()  # 프로세스 동기화를 위한 Lock 객체
+    process = Process(target=increment, args=(lock,))
     process.start()
     processes.append(process)
 
--- multi_thread.py
+++ multi_thread.py
@@ -4,15 +4,15 @@ import threading
 
 
 count = 0  # 전역 변수 초기화
-lock = threading.Lock()  # 스레드 동기화를 위한 Lock 객체
 
 print('------Multi Thread-----')
 
-def increment():
+def increment(lock):
+    lock.acquire()
     global count
     while count < 3000000:
-        with lock:
-            count += 1
+        count += 1
+    lock.release()
     print(f"Thread ID: {threading.get_ident()}, Process ID: {os.getpid()}")
 
 # 총 소요 시간 측정을 위한 시작 시간 기록
@@ -21,7 +21,8 @@ start_time = time.time()
 # 5개의 스레드 생성 및 실행
 threads = []
 for _ in range(5):
-    thread = threading.Thread(target=increment)
+    lock = threading.Lock()  # 스레드 동기화를 위한 Lock 객체
+    thread = threading.Thread(target=increment, args=(lock,))
     thread.start()
     threads.append(thread)
 
