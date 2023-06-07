f89e28fe50a87f8ba0fe636126b66374d39382f9
Taerande <59725436+Taerande@users.noreply.github.com>
2023-06-07 16:36:52 +0900

    while -> for loop 일관성 확보

--- single_thread.py
+++ single_thread.py
@@ -1,15 +1,19 @@
 import os
 import time
 import threading
-
+from math import ceil
 
 count = 0  # 전역 변수 초기화
+max_count = 3000000
+num_process = 1
+for_range = ceil(max_count/num_process)
 
 print('------Single Thread-----')
 
 def increment():
     global count
-    while count < 3000000:
+    for _ in range(for_range):
+      if count < max_count:
         count += 1
     print(f"Thread ID: {threading.get_ident()}, Process ID: {os.getpid()}")
 
@@ -18,7 +22,7 @@ start_time = time.time()
 
 # 5개의 스레드 생성 및 실행
 threads = []
-for _ in range(1):
+for _ in range(num_process):
     thread = threading.Thread(target=increment)
     thread.start()
     threads.append(thread)
