f53ba34e171e3900d969c2557c4a64533df29df7
Taerande <59725436+Taerande@users.noreply.github.com>
2023-06-09 16:01:37 +0900

    python GIL make Thread Lock Automatically

--- multi_thread.py
+++ multi_thread.py
@@ -5,20 +5,17 @@ import math
 
 
 count = 0  # 전역 변수 초기화
-lock = threading.Lock()  # 스레드 동기화를 위한 Lock 객체
 max_count = 3000000
 num_thread = 4
 for_range = math.ceil(max_count / num_thread)
 
 print('------Multi Thread-----')
 
-def increment(lock):
+def increment():
     global count
     for _ in range(for_range):
-      lock.acquire()
       if count < max_count:
         count += 1
-      lock.release()
     print(f"Thread ID: {threading.get_ident()}, Process ID: {os.getpid()}")
 
 # 총 소요 시간 측정을 위한 시작 시간 기록
@@ -27,7 +24,7 @@ start_time = time.time()
 # 5개의 스레드 생성 및 실행
 threads = []
 for _ in range(num_thread):
-    thread = threading.Thread(target=increment, args=(lock,))
+    thread = threading.Thread(target=increment)
     thread.start()
     threads.append(thread)
 
